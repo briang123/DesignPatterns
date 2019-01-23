8aa724a8d9e21a6904f094014f9c1da261079b54
briang123 <briang.sandiego@gmail.com>
2019-01-22 20:37:37 -0800

    Changed to WriteLine

--- Behavioral.Strategy/Program.cs
+++ Behavioral.Strategy/Program.cs
@@ -11,16 +11,16 @@ namespace Behavioral.Strategy
             const int num2 = 5;
 
             var context = new Context(new OperationAdd());
-            Console.Write(@"{0} + {1} = {2}", num1, num2, context.ExecuteStrategy(num1, num2));
+            Console.WriteLine(@"{0} + {1} = {2}", num1, num2, context.ExecuteStrategy(num1, num2));
 
             context = new Context(new OperationSubtract());
-            Console.Write(@"{0} - {1} = {2}", num1, num2, context.ExecuteStrategy(num1, num2));
+            Console.WriteLine(@"{0} - {1} = {2}", num1, num2, context.ExecuteStrategy(num1, num2));
 
             context = new Context(new OperationMultiply());
-            Console.Write(@"{0} * {1} = {2}", num1, num2, context.ExecuteStrategy(num1, num2));
+            Console.WriteLine(@"{0} * {1} = {2}", num1, num2, context.ExecuteStrategy(num1, num2));
 
             context = new Context(new OperationDivide());
-            Console.Write(@"{0} / {1} = {2}", num1, num2, context.ExecuteStrategy(num1, num2));
+            Console.WriteLine(@"{0} / {1} = {2}", num1, num2, context.ExecuteStrategy(num1, num2));
 
             Console.Write(@"Press any key to continue...");
             Console.ReadKey(true);
