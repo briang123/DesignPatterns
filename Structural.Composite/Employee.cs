ed295e6058ac574dfaf7cfc88f3f1df86cda74c1
briang123 <briang.sandiego@gmail.com>
2019-01-23 10:17:16 -0800

    Created Composite design pattern

---
+++ Structural.Composite/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace Structural.Composite
+{
+    /// <summary>
+    /// Step 1 - Create Employee class having l ist of Employee objects
+    /// </summary>
+    public class Employee
+    {
+        private readonly string _name;
+        private readonly string _dept;
+        private readonly int _sal;
+        private readonly List<Employee> _subordinates;
+
+        public Employee(string name, string dept, int sal)
+        {
+            _name = name;
+            _dept = dept;
+            _sal = sal;
+            _subordinates = new List<Employee>();
+        }
+
+        public void Add(Employee e)
+        {
+            _subordinates.Add(e);
+        }
+
+        public void Remove(Employee e)
+        {
+            _subordinates.Remove(e);
+        }
+
+        public List<Employee> GetSubordinates()
+        {
+            return _subordinates;
+        }
+
+        public string GetEmployeeInfo()
+        {
+            return "Employee: [ Name: " + _name + ", Dept: " + _dept + ", Salary: " + _sal + "]";
+        }
+    }
+}
---
+++ Structural.Composite/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Structural.Composite
+{
+    /// <summary>
+    /// Step 2 - Use the Employee class to create and print employee hierarchy
+    /// </summary>
+    internal class Program
+    {
+        private static void Main()
+        {
+
+            var ceo = new Employee("John", "CEO", 20000);
+            var headSales = new Employee("Robert", "Head Sales", 20000);
+            var headMarketing = new Employee("Michael", "Head Marketing", 20000);
+
+            var clerk1 = new Employee("Laura", "Marketing", 10000);
+            var clerk2 = new Employee("Bob", "Marketing", 10000);
+
+            var salesExecutive1 = new Employee("Richard", "Sales", 10000);
+            var salesExecutive2 = new Employee("Rob", "Sales", 10000);
+
+            ceo.Add(headSales);
+            ceo.Add(headMarketing);
+
+            headSales.Add(salesExecutive1);
+            headSales.Add(salesExecutive2);
+
+            headMarketing.Add(clerk1);
+            headMarketing.Add(clerk2);
+
+            Console.WriteLine(ceo.GetEmployeeInfo());
+
+            foreach (var headEmployee in ceo.GetSubordinates())
+            {
+                Console.WriteLine(headEmployee.GetEmployeeInfo());
+
+                foreach (var employee in headEmployee.GetSubordinates())
+                {
+                    Console.WriteLine(employee.GetEmployeeInfo());
+                }
+            }
+
+            Console.Write(@"Press any key to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
